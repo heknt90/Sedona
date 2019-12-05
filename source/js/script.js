2968d17498456e51be243a0e9d75ebc93d31d609
Nikita Efremov <heknt90@gmail.com>
2019-12-05 23:43:19 +0300

    Футер + несколько контейнеров на главной странице + открытие формы бронирования отеля

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,27 @@
 'use strict'
 
-var modalCheckbox = document.querySelector("#toggle-booking-form-checkbox");
+var orderPopup = {
+  toggleButton: document.getElementById('order-button-toggle'),
+  popupForm: document.getElementById('order-form-popup')
+}
+
+// console.log(orderPopup.popupForm.classList);
+
+if (orderPopup.popupForm) {
+  orderPopup.toggleButton.addEventListener('click', toggleOrderForm(orderPopup.popupForm, 'order__form_hidden'), false);
+}
+
+function toggleOrderForm(popup, className) {
+  return function() {
+    popup.classList.toggle(className);
+  }
+}
+
+function check(foo) {
+  return (foo) ? true : false;
+}
+
+// var modalCheckbox = document.querySelector("#toggle-booking-form-checkbox");
 
 // Так как данная реализация кнопки открывается засчет состояния выбора/снятия
 // checkbox'а, то вешать прикреплять данную функцию к кнопке нет необходимости,
@@ -12,12 +33,12 @@ var modalCheckbox = document.querySelector("#toggle-booking-form-checkbox");
 // отключенном JavaScript пользователи не смогут воспользоваться данным модальным окном
 
 // Как вариант п.4.1 можно реализовать в другой ветке git 
-function modalViewToggle_test() {
-  if (modalCheckbox) {
-    modalCheckbox.checked = !modalCheckbox.checked;
-  }
-}
+// function modalViewToggle_test() {
+//   if (modalCheckbox) {
+//     modalCheckbox.checked = !modalCheckbox.checked;
+//   }
+// }
 
-window.onload = function() {
-  modalViewToggle_test();
-}
+// window.onload = function() {
+//   modalViewToggle_test();
+// }
